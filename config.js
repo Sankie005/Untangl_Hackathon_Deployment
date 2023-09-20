40a29a697f224189da4c16abf69c127677058b9d
Sankie005 <89256230+Sankie005@users.noreply.github.com>
2023-09-19 17:05:19 -0700

    java_script_files

---
+++ config.js
@@ -0,0 +1,14 @@
+window.botpressWebChat.init({
+    "composerPlaceholder": "Talk To Bravo!",
+    "botConversationDescription": "Meet Bravo A Mental Health Chatbot",
+    "botId": "451024b4-bc37-4d48-8e2b-8209570d7579",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "451024b4-bc37-4d48-8e2b-8209570d7579",
+    "lazySocket": true,
+    "botName": "Untangl Bravo",
+    "avatarUrl": "https://static.wixstatic.com/media/670530_4acec55128ef4b48be3ef2d9642bee23~mv2.jpg/v1/fill/w_242,h_242,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/9e8e557d-038a-4cca-a33a-4c77f44b99d9.jpg",
+    "website": "untangl.org",
+    "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/032b74da-a2c9-432f-bfa4-56028e9bbf58/v99684/style.css",
+    "frontendVersion": "v1"
+});
